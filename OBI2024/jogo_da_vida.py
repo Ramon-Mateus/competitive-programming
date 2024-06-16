a4de441752b764646cae8e34f247915a2c4d054b
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-06-15 22:42:44 -0300

    add tentativa de resolução (não funciona corretamente)

--- OBI2024/jogo_da_vida.py
+++ OBI2024/jogo_da_vida.py
@@ -14,62 +14,132 @@ for i in range(0, n):
     for j in range(0, n):
         jogo[i].append(row[j])
 
+def check(i, j, vi):
+    if jogo[i][j] == 0:
+        if vi == 3:
+            jogoQ1[i].append(1)
+        else:
+            jogoQ1[i].append(0)
+    else:
+        if(vi == 2 or vi == 3):
+            jogoQ1[i].append(1)
+        else:
+            jogoQ1[i].append(0)
+
 for i in range(0, n):
     jogoQ1.append([])
     for j in range(0, n):
         if i == 0:
             if j == 0:
-                if jogo[i,j+1] == 1:
+                if jogo[i][j+1] == 1:
                     v += 1
-                if jogo[i+1,j+1] == 1:
+                if jogo[i+1][j+1] == 1:
                     v += 1
-                if jogo[i+1,j] == 1:
+                if jogo[i+1][j] == 1:
                     v += 1
-                if jogo[i][j] == 0:
-                    if v == 3:
-                        jogoQ1[i][j] = 1
-                    else:
-                        jogoQ1[i][j] = 0
-                else:
-                    if(v == 2 or v == 3):
-                        jogoQ1[i][j] = 1
-                    else:
-                        jogoQ1[i][j] = 0
+                check(i, j, v)
+                v = 0
             elif j == n - 1:
-                if jogo[i,j-1] == 1:
+                if jogo[i][j-1] == 1:
                     v += 1
-                if jogo[i-1,j-1] == 1:
+                if jogo[i+1][j-1] == 1:
                     v += 1
-                if jogo[i-1,j] == 1:
+                if jogo[i+1][j] == 1:
                     v += 1
-                if jogo[i][j] == 0:
-                    if v == 3:
-                        jogoQ1[i][j] = 1
-                    else:
-                        jogoQ1[i][j] = 0
-                else:
-                    if(v == 2 or v == 3):
-                        jogoQ1[i][j] = 1
-                    else:
-                        jogoQ1[i][j] = 0
+                check(i, j, v)
+                v = 0
             else:
-                if jogo[i,j+1] == 1:
+                if jogo[i][j+1] == 1:
                     v += 1
-                if jogo[i+1,j+1] == 1:
+                if jogo[i+1][j+1] == 1:
                     v += 1
-                if jogo[i+1,j] == 1:
+                if jogo[i+1][j] == 1:
                     v += 1
-                if jogo[i,j-1] == 1:
+                if jogo[i+1][j-1] == 1:
                     v += 1
-                if jogo[i-1,j-1] == 1:
+                if jogo[i][j-1] == 1:
                     v += 1
-                if jogo[i][j] == 0:
-                    if v == 3:
-                        jogoQ1[i][j] = 1
-                    else:
-                        jogoQ1[i][j] = 0
-                else:
-                    if(v == 2 or v == 3):
-                        jogoQ1[i][j] = 1
-                    else:
-                        jogoQ1[i][j] = 0
+                check(i, j, v)
+                v = 0
+        elif i == n - 1:
+            if j == 0:
+                if jogo[i-1][j] == 1:
+                    v += 1
+                if jogo[i-1][j+1] == 1:
+                    v += 1
+                if jogo[i][j+1] == 1:
+                    v += 1
+                check(i, j, v)
+                v = 0
+            elif j == n - 1:
+                if jogo[i][j-1] == 1:
+                    v += 1
+                if jogo[i-1][j-1] == 1:
+                    v += 1
+                if jogo[i-1][j] == 1:
+                    v += 1
+                check(i, j, v)
+                v = 0
+            else:
+                if jogo[i][j-1] == 1:
+                    v += 1
+                if jogo[i-1][j-1] == 1:
+                    v += 1
+                if jogo[i-1][j] == 1:
+                    v += 1
+                if jogo[i-1][j+1] == 1:
+                    v += 1
+                if jogo[i][j+1] == 1:
+                    v += 1
+                check(i, j, v)
+                v = 0
+        elif j == 0:
+            if jogo[i-1][j] == 1:
+                v += 1
+            if jogo[i-1][j+1] == 1:
+                v += 1
+            if jogo[i][j+1] == 1:
+                v += 1
+            if jogo[i+1][j+1] == 1:
+                v += 1
+            if jogo[i+1][j] == 1:
+                v += 1
+            check(i, j, v)
+            v = 0
+        elif j == n - 1:
+            if jogo[i-1][j] == 1:
+                v += 1
+            if jogo[i-1][j-1] == 1:
+                v += 1
+            if jogo[i][j-1] == 1:
+                v += 1
+            if jogo[i+1][j-1] == 1:
+                v += 1
+            if jogo[i+1][j] == 1:
+                v += 1
+            check(i, j, v)
+            v = 0
+        else:
+            if jogo[i-1][j-1] == 1:
+                v += 1
+            if jogo[i-1][j] == 1:
+                v += 1
+            if jogo[i-1][j+1] == 1:
+                v += 1
+            if jogo[i][j+1] == 1:
+                v += 1
+            if jogo[i+1][j+1] == 1:
+                v += 1
+            if jogo[i+1][j] == 1:
+                v += 1
+            if jogo[i+1][j-1] == 1:
+                v += 1
+            if jogo[i][j-1] == 1:
+                v += 1
+            check(i, j, v)
+            v = 0
+
+for k in range(0, n):
+    for w in range(0, n-1):
+        print(jogoQ1[k][w], end="")
+    print(jogoQ1[k][n-1])
