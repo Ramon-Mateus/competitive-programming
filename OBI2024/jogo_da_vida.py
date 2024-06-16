aca3d0edac31aefe27cac4a8091f043887c0e642
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-06-16 18:24:58 -0300

    resolução jogo da vida Q-1

--- OBI2024/jogo_da_vida.py
+++ OBI2024/jogo_da_vida.py
@@ -15,7 +15,7 @@ for i in range(0, n):
         jogo[i].append(row[j])
 
 def check(i, j, vi):
-    if jogo[i][j] == 0:
+    if jogo[i][j] == "0":
         if vi == 3:
             jogoQ1[i].append(1)
         else:
@@ -31,113 +31,115 @@ for i in range(0, n):
     for j in range(0, n):
         if i == 0:
             if j == 0:
-                if jogo[i][j+1] == 1:
+                if jogo[i][j+1] == "1":
                     v += 1
-                if jogo[i+1][j+1] == 1:
+                if jogo[i+1][j+1] == "1":
                     v += 1
-                if jogo[i+1][j] == 1:
+                if jogo[i+1][j] == "1":
                     v += 1
                 check(i, j, v)
                 v = 0
             elif j == n - 1:
-                if jogo[i][j-1] == 1:
+                if jogo[i][j-1] == "1":
                     v += 1
-                if jogo[i+1][j-1] == 1:
+                if jogo[i+1][j-1] == "1":
                     v += 1
-                if jogo[i+1][j] == 1:
+                if jogo[i+1][j] == "1":
                     v += 1
                 check(i, j, v)
                 v = 0
             else:
-                if jogo[i][j+1] == 1:
+                if jogo[i][j+1] == "1":
                     v += 1
-                if jogo[i+1][j+1] == 1:
+                if jogo[i+1][j+1] == "1":
                     v += 1
-                if jogo[i+1][j] == 1:
+                if jogo[i+1][j] == "1":
                     v += 1
-                if jogo[i+1][j-1] == 1:
+                if jogo[i+1][j-1] == "1":
                     v += 1
-                if jogo[i][j-1] == 1:
+                if jogo[i][j-1] == "1":
                     v += 1
                 check(i, j, v)
                 v = 0
         elif i == n - 1:
             if j == 0:
-                if jogo[i-1][j] == 1:
+                if jogo[i-1][j] == "1":
                     v += 1
-                if jogo[i-1][j+1] == 1:
+                if jogo[i-1][j+1] == "1":
                     v += 1
-                if jogo[i][j+1] == 1:
+                if jogo[i][j+1] == "1":
                     v += 1
                 check(i, j, v)
                 v = 0
             elif j == n - 1:
-                if jogo[i][j-1] == 1:
+                if jogo[i][j-1] == "1":
                     v += 1
-                if jogo[i-1][j-1] == 1:
+                if jogo[i-1][j-1] == "1":
                     v += 1
-                if jogo[i-1][j] == 1:
+                if jogo[i-1][j] == "1":
                     v += 1
                 check(i, j, v)
                 v = 0
             else:
-                if jogo[i][j-1] == 1:
+                if jogo[i][j-1] == "1":
                     v += 1
-                if jogo[i-1][j-1] == 1:
+                if jogo[i-1][j-1] == "1":
                     v += 1
-                if jogo[i-1][j] == 1:
+                if jogo[i-1][j] == "1":
                     v += 1
-                if jogo[i-1][j+1] == 1:
+                if jogo[i-1][j+1] == "1":
                     v += 1
-                if jogo[i][j+1] == 1:
+                if jogo[i][j+1] == "1":
                     v += 1
                 check(i, j, v)
                 v = 0
         elif j == 0:
-            if jogo[i-1][j] == 1:
+            if jogo[i-1][j] == "1":
                 v += 1
-            if jogo[i-1][j+1] == 1:
+            if jogo[i-1][j+1] == "1":
                 v += 1
-            if jogo[i][j+1] == 1:
+            if jogo[i][j+1] == "1":
                 v += 1
-            if jogo[i+1][j+1] == 1:
+            if jogo[i+1][j+1] == "1":
                 v += 1
-            if jogo[i+1][j] == 1:
+            if jogo[i+1][j] == "1":
                 v += 1
             check(i, j, v)
             v = 0
         elif j == n - 1:
-            if jogo[i-1][j] == 1:
+            if jogo[i-1][j] == "1":
                 v += 1
-            if jogo[i-1][j-1] == 1:
+            if jogo[i-1][j-1] == "1":
                 v += 1
-            if jogo[i][j-1] == 1:
+            if jogo[i][j-1] == "1":
                 v += 1
-            if jogo[i+1][j-1] == 1:
+            if jogo[i+1][j-1] == "1":
                 v += 1
-            if jogo[i+1][j] == 1:
+            if jogo[i+1][j] == "1":
                 v += 1
             check(i, j, v)
             v = 0
         else:
-            if jogo[i-1][j-1] == 1:
+            if jogo[i-1][j-1] == "1":
                 v += 1
-            if jogo[i-1][j] == 1:
+            if jogo[i-1][j] == "1":
                 v += 1
-            if jogo[i-1][j+1] == 1:
+            if jogo[i-1][j+1] == "1":
                 v += 1
-            if jogo[i][j+1] == 1:
+            if jogo[i][j+1] == "1":
                 v += 1
-            if jogo[i+1][j+1] == 1:
+            if jogo[i+1][j+1] == "1":
                 v += 1
-            if jogo[i+1][j] == 1:
+            if jogo[i+1][j] == "1":
                 v += 1
-            if jogo[i+1][j-1] == 1:
+            if jogo[i+1][j-1] == "1":
                 v += 1
-            if jogo[i][j-1] == 1:
+            if jogo[i][j-1] == "1":
                 v += 1
             check(i, j, v)
             v = 0
+
+print("----------------")
 
 for k in range(0, n):
     for w in range(0, n-1):
